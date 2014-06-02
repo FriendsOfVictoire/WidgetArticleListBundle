cde20cde4b292de6b21d988ae03f1c7bb8209852
Thomas Beaujean <thomas@appventus.com>
2014-06-02 09:44:14 +0200

    [VICMS-180] refactor victoire

--- Entity/WidgetArticleList.php
+++ Entity/WidgetArticleList.php
@@ -12,8 +12,6 @@ use Victoire\Bundle\CoreBundle\Entity\Widget;
  */
 class WidgetArticleList extends Widget
 {
-    use \Victoire\Bundle\CoreBundle\Entity\Traits\WidgetTrait;
-
     /**
      * @var string
      *
